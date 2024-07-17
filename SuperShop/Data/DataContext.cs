fc9f1cf0a74c7ea7caf89b56f3eb3808f24f5b80
AnaGuerra09 <anadesouguerra@gmail.com>
2024-07-17 22:05:18 +0100

    First controller

---
+++ SuperShop/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SuperShop.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {   
+        }
+    }
+}
